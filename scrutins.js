30df37751b1a21023f26742b40441cc515d1f4c6
Valouleloup <gohan_3_7@hotmail.com>
2020-12-03 11:23:15 +0100

    Edit colors, new data

---
+++ scrutins.js
@@ -0,0 +1,54 @@
+var scrutins = {
+    578: {
+        titre: "l'ensemble du projet de loi pour une immigration maîtrisée, un droit d'asile effectif et une intégration réussie (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/immigration_maitrisee_droit_asile_effectif?etape=15-AN1-DEPOT",
+    },
+    1432: {
+        titre: "l'ensemble de la proposition de loi organique relative à la lutte contre la manipulation de l'information (lecture définitive).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/lutte_fausses_informations?etape=15-AN1-DEPOT",
+    },
+    1662: {
+        titre: "l'ensemble de la proposition de loi visant à prévenir les violences lors des manifestations et à sanctionner leurs auteurs (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/prevention_sanction_violences_manifestations?etape=15-SN1-DEPOT",
+    },
+    1814: {
+        titre: "l'ensemble du projet de loi relatif à l'organisation et à la transformation du système de santé (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/organisation_transformation_systeme_sante?etape=15-AN1-DEPOT",
+    },
+    1923: {
+        titre: "l'ensemble du projet de loi de transformation de la fonction publique (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/transformation_fonction_publique?etape=15-AN1-DEPOT",
+    },
+    2059: {
+        titre: "l'ensemble du projet de loi autorisant la ratification de l'accord économique et commercial global entre l'Union européenne et ses États membres, d'une part, et le Canada... (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/aecg_partenariat_strategique_ue-canada?etape=15-AN1-DEPOT",
+    },
+    2071: {
+        titre: "l'ensemble du projet de loi d'orientation des mobilités (nouvelle lecture). ",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/loi_orientation_mobilites?etape=15-SN1-DEPOT",
+    },
+    2742: {
+        titre: "l'ensemble de la proposition de loi visant à lutter contre les contenus haineux sur internet (lecture définitive).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/lutte_contre_haine_internet?etape=15-AN1-DEPOT",
+    },
+    2749: {
+        titre: "l'ensemble de la proposition de loi permettant le don de congés payés sous forme de chèques-vacances aux membres du secteur médico-social en reconnaissance de leur action durant l'épidémie de covid-19 (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/don_conges_cheques-vacances_secteur_medico-social_covid-19?etape=15-AN1-DEPOT",
+    },
+    2762: {
+        titre: "l'article unique de la proposition de résolution visant à rendre l'accès aux masques gratuit (article 34-1 de la Constitution).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/acces_masques_gratuits?etape=15-ANLUNI-DEPOT",
+    },
+    2940: {
+        titre: "l'ensemble du projet de loi relatif aux conditions de mise sur le marché de certains produits phytopharmaceutiques en cas de danger sanitaire pour les betteraves sucrières (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/mise_sur_le_marche_de_certains_produits_phytopharmaceutiques_en_cas_de_danger_sanitaire?etape=15-AN1-DEPOT",
+    },
+    3186: {
+        titre: "l'ensemble du projet de loi de finances pour 2021 (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/loi_de_finances_pour_2021?etape=15-AN1-DEPOT",
+    },
+    3254: {
+        titre: "l'ensemble de la proposition de loi relative à la sécurité globale (première lecture).",
+        url: "http://www.assemblee-nationale.fr/dyn/15/dossiers/securite_globale1?etape=15-AN1-DEPOT",
+    },
+}
